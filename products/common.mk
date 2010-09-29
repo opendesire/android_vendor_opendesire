0a45ae54a5bd283ece2c6358c4a1f3e71db6cc4d
Adam Green <greena88@gmail.com>
2010-09-29 03:00:59 +0100

    Bump version v4.0.33

--- products/common.mk
+++ products/common.mk
@@ -6,7 +6,7 @@ ifdef OPENDESIRE_NIGHTLY
         ro.modversion=OpenDesire-4-$(shell date +%m%d%Y)-NIGHTLY
 else
     PRODUCT_PROPERTY_OVERRIDES += \
-        ro.modversion=OpenDesire-4.0.32
+        ro.modversion=OpenDesire-4.0.33
 endif
 
 # Set default ringtone
